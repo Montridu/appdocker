f4dd96d90e86aa58792ecb2b504bc620acf1bf6a
Montridu <Montri.du@rutnin.com>
2024-12-05 14:46:17 +0700

    aa

---
+++ index.js
@@ -0,0 +1,8 @@
+const http = require('http')
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello Borntodev\n');
+  });
+  server.listen(4000, () => {
+    console.log(`Server is running on port 4000`);
+  });
