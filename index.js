3d30d8416572b47e65dc03930b1580967e1b0065
Montridu <Montri.du@rutnin.com>
2025-01-06 16:35:16 +0700

    Edits

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ const server = http.createServer((req, res) => {
     res.end('Hello Borntodev\n');
   });
   server.listen(4000, () => {
-    console.log(`Server is running on port 4000`);
+    console.log(`Server is running on portxxxxxx 4000`);
   });
